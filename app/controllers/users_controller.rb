b6a88890a1539539b28565f2729e309575be7154
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-27 17:23:15 -0600

    Add create new user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    user = User.new(user_params)
+    if user.save
+      flash[:success] = "You are now registered and logged in."
+      redirect_to '/profile'
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+  def user_params
+    params.permit(:name, :address, :city, :state, :zip, :email, :password)
   end
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,25 +1,3 @@
-# User Story 10, User Registration
-#
-# As a visitor
-# When I click on the 'register' link in the nav bar
-# Then I am on the user registration page ('/register')
-# And I see a form where I input the following data:
-# - my name
-# - my street address
-# - my city
-# - my state
-# - my zip code
-# - my email address
-# - my preferred password
-# - a confirmation field for my password
-#
-# When I fill in this form completely,
-# And with a unique email address not already in the system
-# My details are saved in the database
-# Then I am logged in as a registered user
-# I am taken to my profile page ("/profile")
-# I see a flash message indicating that I am now registered and logged in
-
 require 'rails_helper'
 
 describe "as a visitor" do
