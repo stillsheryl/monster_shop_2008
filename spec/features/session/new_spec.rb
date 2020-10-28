8551772142d07dfa598584c3b44019e0f5db740d
Curtis Bartell <cbartell980@gmail.com>
2020-10-28 17:51:59 -0600

    Add tests for user redirects from '/login'

--- spec/features/session/new_spec.rb
+++ spec/features/session/new_spec.rb
@@ -63,4 +63,69 @@ describe "As a visitor" do
       end
     end
   end
+  describe "As a user" do
+    describe "who is logged in and tries to visit the login path" do
+      it "I am redirected to my profile page" do
+        kiera = User.create!(name: 'Kiera Allen', address: '124 Main St.', city: 'Denver', state: 'CO', zip: 80205, email: 'bob@marley.com', password: 'password')
+        visit '/login'
+
+        fill_in :email, with: kiera.email
+        fill_in :password, with: kiera.password
+
+        click_button 'Login'
+
+        visit '/login'
+
+
+        expect(current_path).to eq('/profile')
+        expect(page).to have_content('You are already logged in.')
+      end
+    end
+  end
+  describe "As a merchant" do
+    describe "who is logged in and tries to visit the login path" do
+      it "I am redirected to my profile page" do
+        sally = User.create!(name: 'Sally Peach', address: '432 Grove St.', city: 'Denver', state: 'CO', zip: 80205, email: 'sally@peach.com', password: 'password', role: 1)
+
+        visit '/login'
+
+        fill_in :email, with: sally.email
+        fill_in :password, with: sally.password
+
+        click_button 'Login'
+
+        visit '/login'
+
+
+        expect(current_path).to eq('/merchant')
+        expect(page).to have_content('You are already logged in.')
+      end
+    end
+  end
+  describe "As an admin" do
+    it "I am redirected to my profile page" do
+      bob = User.create!(name: 'Bob Ross', address: '745 Rose St.', city: 'Denver', state: 'CO', zip: 80205, email: 'bob@ross.com', password: 'password', role: 2)
+
+      visit '/login'
+
+      fill_in :email, with: bob.email
+      fill_in :password, with: bob.password
+
+      click_button 'Login'
+
+      visit '/login'
+
+
+      expect(current_path).to eq('/admin')
+      expect(page).to have_content('You are already logged in.')
+    end
+  end
 end
+# User Story 15, Users who are logged in already are redirected
+#
+# As a registered user, merchant, or admin
+# When I visit the login path
+# If I am a regular user, I am redirected to my profile page
+# If I am a merchant user, I am redirected to my merchant dashboard page
+# If I am an admin user, I am redirected to my admin dashboard page
+# And I see a flash message that tells me I am already logged in
