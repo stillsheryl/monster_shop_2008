1ff36898f2c19035fb2cf36d09a9b03016155e0f
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-28 16:50:44 -0600

    Add success message when logged in successfully

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      flash[:success] = "You are now logged in."
       if @user.user?
         redirect_to '/profile'
       elsif @user.merchant?
