c8e36fe1eb541b4c6248e6f9b285c6370645dd74
Jose Lopez <joselopez11394@gmail.com>
2020-10-27 22:35:19 -0400

    adds story 12 with test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,17 @@
 class UsersController < ApplicationController
   def new
-    @form_info = user_params
+
   end
 
   def create
     user = User.new(user_params)
+    @params = user_params
     if user.save
       flash[:success] = "You are now registered and logged in."
       redirect_to '/profile'
     else
       flash[:errors] = user.errors.full_messages.uniq.to_sentence
-      # if flash[:notice].include?('Email has already been taken')
-      #   user.update(email: nil)
-      # render :new
-      redirect_to '/register'
+      render :new
     end
   end
 
