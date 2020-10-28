0261ed42c5a8338dd32038dffbaae99107c4a074
Jose Lopez <joselopez11394@gmail.com>
2020-10-28 15:18:01 -0400

    adds test for addin role to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,30 @@ RSpec.describe User do
     it { should validate_presence_of :password }
 
     it { should validate_uniqueness_of :email }
-    
+
     it { should validate_confirmation_of :password }
+  end
+
+  describe "roles" do
+    it "can be created as a default user" do
+      User.create!(email: 'kiera@gmail.com', password: 'password', role: 0)
+
+      expect(user.role).to eq("user")
+      expect(user.user?).to be_truthy
+    end
+
+    it "can be created as a default user" do
+      User.create!(email: 'kiera@gmail.com', password: 'password', role: 1)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+
+    it "can be created as a default user" do
+      User.create!(email: 'kiera@gmail.com', password: 'password', role: 2)
+
+      expect(user.role).to eq("merchant")
+      expect(user.merchant?).to be_truthy
+    end
   end
 end
