c5c3c6668185856a8edb803188d23b37accf35ff
Curtis Bartell <cbartell980@gmail.com>
2020-10-27 18:04:14 -0600

    Add user registration sad path tests

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -19,5 +19,21 @@ describe "as a visitor" do
       expect(current_path).to eq("/profile")
       expect(page).to have_content("You are now registered and logged in.")
     end
+    it "will show an error message if the user does not fill out the form completely" do
+      visit '/register'
+
+      fill_in :address, with: "123 Main St."
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: "80205"
+      fill_in :email, with: "bob@marley.com"
+      fill_in :password, with: "password"
+      fill_in :confirmation, with: "password"
+
+      click_button "Register"
+
+      expect(page).to have_content("Name can't be blank")
+      expect(current_path).to eq('/register')
+    end
   end
 end
