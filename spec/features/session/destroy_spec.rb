aae05bcf5236329625c8ca7fa555318b08139728
Curtis Bartell <cbartell980@gmail.com>
2020-10-28 19:03:57 -0600

    Add test for cart being emptied on logout

--- spec/features/session/destroy_spec.rb
+++ spec/features/session/destroy_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 #
 # User Story 16, User can log out
-#
-# As a registered user, merchant, or admin
-# When I visit the logout path
-# I am redirected to the welcome / home page of the site
-# And I see a flash message that indicates I am logged out
 # Any items I had in my shopping cart are deleted
 
 describe "As a registered user, merchant, or admin" do
@@ -24,6 +19,30 @@ describe "As a registered user, merchant, or admin" do
 
       expect(current_path).to eq('/home')
       expect(page).to have_content('You are now logged out.')
+    end
+    it "any items in my cart are deleted" do
+      mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
+      pencil = mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      kiera = User.create!(name: 'Kiera Allen', address: '124 Main St.', city: 'Denver', state: 'CO', zip: 80205, email: 'bob@marley.com', password: 'password')
+
+      visit '/login'
+
+      fill_in :email, with: kiera.email
+      fill_in :password, with: kiera.password
+
+      click_button 'Login'
+
+      visit "/items/#{paper.id}"
+      click_on "Add To Cart"
+
+      page.driver.submit :delete, '/logout', {}
+
+      save_and_open_page
+
+      within 'nav' do
+        expect(page).to have_content("Cart: 0")
+      end
     end
   end
 end
