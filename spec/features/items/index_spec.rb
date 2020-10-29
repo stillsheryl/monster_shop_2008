21af17fbba0032359b8385e1d63d5304df748565
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-29 16:46:48 -0600

    Update tests to not look for nonexistant css

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -45,15 +45,12 @@ RSpec.describe "Items Index Page" do
         expect(page).to have_css("img[src*='#{@pull_toy.image}']")
       end
 
-      within "#item-#{@dog_bone.id}" do
-        expect(page).to_not have_link(@dog_bone.name)
-        expect(page).to_not have_content(@dog_bone.description)
-        expect(page).to_not have_content("Price: $#{@dog_bone.price}")
-        expect(page).to_not have_content("Inactive")
-        expect(page).to_not have_content("Inventory: #{@dog_bone.inventory}")
-        expect(page).to_not have_link(@brian.name)
-        expect(page).to_not have_css("img[src*='#{@dog_bone.image}']")
-      end
+      expect(page).to_not have_link(@dog_bone.name)
+      expect(page).to_not have_content(@dog_bone.description)
+      expect(page).to_not have_content("Price: $#{@dog_bone.price}")
+      expect(page).to_not have_content("Inactive")
+      expect(page).to_not have_content("Inventory: #{@dog_bone.inventory}")
+
     end
   end
 end
