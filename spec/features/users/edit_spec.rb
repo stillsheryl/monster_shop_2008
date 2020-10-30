e688e5d878a3cd96a039f9535462652560037e4d
Curtis Bartell <cbartell980@gmail.com>
2020-10-30 11:45:53 -0600

    Update edit profile tests with expectations for updated info

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -4,6 +4,7 @@ describe "As a registered User" do
   describe "when I visit my profile page" do
     before(:each) do
       @kiera = User.create!(name: 'Kiera Allen', address: '124 Main St.', city: 'Denver', state: 'CO', zip: 80205, email: 'bob@marley.com', password: 'password', role: 0)
+      @jose = { name: "Jose Lopez", address: "125 Candy Cane Ln.", city: "Miami", state: "FL", zip: 54321, email: "bob@marley.com" }
 
       visit '/login'
 
@@ -33,12 +34,12 @@ describe "As a registered User" do
         # click_link "Edit Profile"
         visit '/profile/edit'
 
-        fill_in :name, with: "Jose Lopez"
-        fill_in :address, with: "125 Candy Cane Ln."
-        fill_in :city, with: "Miami"
-        fill_in :state, with: "FL"
-        fill_in :zip, with: 54321
-        fill_in :email, with: "bob@marley.com"
+        fill_in :name, with: @jose[:name]
+        fill_in :address, with: @jose[:address]
+        fill_in :city, with: @jose[:city]
+        fill_in :state, with: @jose[:state]
+        fill_in :zip, with: @jose[:zip]
+        fill_in :email, with: @jose[:email]
         fill_in :password, with: @kiera.password
 
         click_button 'Submit'
@@ -46,10 +47,14 @@ describe "As a registered User" do
         expect(current_path).to eq('/profile')
         expect(page).to have_content('Profile information updated.')
 
-        # Add tests for updated info on profile
-        # expect(page).to have_content()
+        expect(page).to have_content(@jose[:name])
+        expect(page).to have_content(@jose[:address])
+        expect(page).to have_content(@jose[:city])
+        expect(page).to have_content(@jose[:state])
+        expect(page).to have_content(@jose[:zip])
+        expect(page).to have_content(@jose[:email])
 
-        # click_link "Edit Profile"
+        click_link "Edit Profile"
         visit '/profile/edit'
 
         fill_in :name, with: ""
