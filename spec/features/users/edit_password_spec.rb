f2253a3ef4fa20b555648e9574b81f3b83e281b7
Kiera <kieraaallen@gmail.com>
2020-10-31 13:53:37 -0600

    Update edit_password_spec.rb

--- spec/features/users/edit_password_spec.rb
+++ spec/features/users/edit_password_spec.rb
@@ -1,17 +1,5 @@
 require 'rails_helper'
 
-# User Story 21, User Can Edit their Password
-#
-# As a registered user
-# When I visit my profile page
-# I see a link to edit my password
-# When I click on the link to edit my password
-# I see a form with fields for a new password, and a new password confirmation
-
-# When I fill in the same password in both fields
-# And I submit the form
-# Then I am returned to my profile page
-# And I see a flash message telling me that my password is updated
 describe 'As a registered user' do
   describe 'When I visit my profile page' do
     before(:each) do
