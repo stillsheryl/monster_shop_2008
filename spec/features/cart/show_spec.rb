48da4e074fd1d9c2a80abf13caefa809ee6c3b2f
Kiera Allen <kieraaallen@gmail.com>
2020-10-31 18:22:26 -0600

    Add Register and Log In links for visitor on checkout page

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Cart show' do
         @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
         @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-        @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
+        @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
         @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
         visit "/items/#{@paper.id}"
         click_on "Add To Cart"
@@ -55,8 +55,82 @@ RSpec.describe 'Cart show' do
 
         expect(page).to have_content("Total: $124")
       end
+
+      it "has a button next to each item to increase the count of items" do
+        visit '/cart'
+
+        @items_in_cart.each do |item|
+          within "#cart-item-#{item.id}" do
+            expect(page).to have_button("+")
+          end
+        end
+
+        within "#cart-item-#{@paper.id}" do
+          click_button "+"
+        end
+
+        expect(page).to have_content("Cart: 4")
+      end
+
+      it "cannot add more items than in inventory" do
+        visit '/cart'
+
+        within "#cart-item-#{@paper.id}" do
+          click_button "+"
+          click_button "+"
+          click_button "+"
+        end
+
+        expect(page).to have_content("There are no more items left in inventory")
+      end
+
+      it "has a button next to each item to decrease the count of items" do
+        visit '/cart'
+
+        @items_in_cart.each do |item|
+          within "#cart-item-#{item.id}" do
+            expect(page).to have_button("-")
+          end
+        end
+
+        within "#cart-item-#{@paper.id}" do
+          click_button "-"
+        end
+
+        expect(page).to have_content("Cart: 2")
+      end
+
+      it "will remove item if quantity gets to 0" do
+        visit '/cart'
+
+        within "#cart-item-#{@paper.id}" do
+          click_button "-"
+        end
+
+        expect(page).to_not have_content(@paper.name)
+      end
+
+      it "I must register or login before checking out" do
+        visit '/cart'
+
+        within("#must-login") do
+          expect(page).to have_content("Please Register or Log In to checkout.")
+          expect(page).to have_link("Register")
+          expect(page).to have_link("Log In")
+        end
+        save_and_open_page
+
+        click_on "Register"
+        expect(current_path).to eq('/register')
+
+        visit '/cart'
+
+        click_on "Log In"
+        expect(current_path).to eq('/login')
+      end
     end
   end
+
   describe "When I haven't added anything to my cart" do
     describe "and visit my cart show page" do
       it "I see a message saying my cart is empty" do
