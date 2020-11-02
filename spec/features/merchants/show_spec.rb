1c6584bcdcc760d7d1b8d7e24ea584e4e4a4ffc3
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-01 20:23:31 -0700

    Update test to try factory bot and faker

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -3,24 +3,24 @@ require 'rails_helper'
 RSpec.describe 'merchant show page', type: :feature do
   describe 'As a user' do
     before :each do
-      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 23137)
+      @shop = create(:merchant)
     end
 
     it 'I can see a merchants name, address, city, state, zip' do
-      visit "/merchants/#{@bike_shop.id}"
+      visit "/merchants/#{@shop.id}"
 
-      expect(page).to have_content("Brian's Bike Shop")
-      expect(page).to have_content("123 Bike Rd.\nRichmond, VA 23137")
+      expect(page).to have_content(@shop.name)
+      expect(page).to have_content(@shop.address)
     end
 
     it 'I can see a link to visit the merchant items' do
-      visit "/merchants/#{@bike_shop.id}"
+      visit "/merchants/#{@shop.id}"
 
-      expect(page).to have_link("All #{@bike_shop.name} Items")
+      expect(page).to have_link("All #{@shop.name} Items")
 
-      click_on "All #{@bike_shop.name} Items"
+      click_on "All #{@shop.name} Items"
 
-      expect(current_path).to eq("/merchants/#{@bike_shop.id}/items")
+      expect(current_path).to eq("/merchants/#{@shop.id}/items")
     end
 
   end
