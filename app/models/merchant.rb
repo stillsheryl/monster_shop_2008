ed57e800f185834fd6945a9841ac9f0a5626818d
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-03 15:13:31 -0700

    Add order relationship to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,7 @@ class Merchant <ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :item_orders, through: :items
   has_many :users, -> {where role: :merchant}
+  has_many :orders, through: :item_orders
 
   validates_presence_of :name,
                         :address,
