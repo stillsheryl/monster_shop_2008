01f179d63dabca293337e0aa262da37486d394c1
Kiera Allen <kieraaallen@gmail.com>
2020-11-05 07:56:21 -0700

    Only items from my merchant are shown in order

--- spec/features/merchant/orders/show_spec.rb
+++ spec/features/merchant/orders/show_spec.rb
@@ -3,20 +3,24 @@ require 'rails_helper'
 RSpec.describe 'merchant order show page' do
   describe 'As a merchant employee when I visit my merchant order show page (such as "/merchant/orders/15")' do
     before :each do
-      @bike_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+      @dog_shop = Merchant.create!(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+      @bike_shop = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
-      @pull_toy = @bike_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-      @dog_bone = @bike_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+      @pull_toy = @dog_shop.items.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+      @dog_bone = @dog_shop.items.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+      @tire = @bike_shop.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
 
       @kiera = User.create!(name: 'Kiera Allen', address: '124 Main St.', city: 'Denver', state: 'CO', zip: 80205, email: 'bobmarley.com', password: 'password')
+
       @order_1 = @kiera.orders.create!(name: 'Kiera Allen', address: '124 Main St.', city: 'Denver', state: 'CO', zip: 80205, user_id: @kiera.id)
       @order_2 = @kiera.orders.create!(name: 'Kiera Allen', address: '124 Main St.', city: 'Denver', state: 'CO', zip: 80205, user_id: @kiera.id)
 
       @item_order_1 = @order_1.item_orders.create!(item_id: @pull_toy.id, price: 4.50, quantity: 2)
       @item_order_2 = @order_1.item_orders.create!(item_id: @dog_bone.id, price: 7.00, quantity: 1)
       @item_order_3 = @order_2.item_orders.create!(item_id: @dog_bone.id, price: 7.00, quantity: 5)
+      @item_order_4 = @order_1.item_orders.create!(item_id: @tire.id, price: 8.00, quantity: 4)
 
-      @sally = User.create!(name: 'Sally Peach', address: '432 Grove St.', city: 'Denver', state: 'CO', zip: 80205, email: 'sallypeach.com', password: 'password', role: 1, merchant_id: @bike_shop.id)
+      @sally = User.create!(name: 'Sally Peach', address: '432 Grove St.', city: 'Denver', state: 'CO', zip: 80205, email: 'sallypeach.com', password: 'password', role: 1, merchant_id: @dog_shop.id)
 
       visit '/login'
 
@@ -49,12 +53,18 @@ RSpec.describe 'merchant order show page' do
       expect(page).to have_content(@kiera.state)
       expect(page).to have_content(@kiera.zip)
     end
+
+    it "only shows the items in the order purchased from my merchant" do
+
+      visit "/merchant/orders/#{@order_1.id}"
+
+      expect(page).to_not have_content(@tire.name)
+    end
   end
 end
 
 # User Story 49, Merchant sees an order show page
 
-# I only see the items in the order that are being purchased from my merchant
 # I do not see any items in the order being purchased from other merchants
 # For each item, I see the following information:
 # - the name of the item, which is a link to my item's show page
