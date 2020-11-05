846ccfa3600f520754e42b5c008cfc8b56d68f5c
Curtis Bartell <cbartell980@gmail.com>
2020-11-04 18:02:19 -0700

    WIP admin user index tests

---
+++ spec/features/admin/users/index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "As an admin user" do
+  describe "I see a 'Users' link in the nav bar" do
+    it " that is only visible to an admin" do
+
+    end
+    describe "When I click on the 'Users' link" do
+      it "my current URL route is '/admin/users', which only " do
+
+      end
+    end
+  end
+end
+describe "As a regular User" do
+  it "I don't see a 'Users' link in the nav bar" do
+
+  end
+end
+
+describe "As a merchant User" do
+  it "I don't see a 'Users' link in the nav bar" do
+
+  end
+end
+
+describe "As a visitor" do
+
+end
